1fae89bfa1b8991aef0548d1c2d4765c7a1dde15
Victoria Melamed <victoriamelamed@MacBook-Pro.local>
2019-12-11 22:12:24 -0800

    made planet disappear and relocate when ship gets too close (lab 1203)

--- Lab 1203 Planet and Ships/sketch.js
+++ Lab 1203 Planet and Ships/sketch.js
@@ -3,37 +3,30 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var ships = []; //declares array
-var planet; //declares the main ball
-//var planet2;
-//var planet2;
+var planet; //declares the planet
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-
-  loadObjects(1);
-
+  loadObjects(1);//prepares the planet and ship(s)
 }
 
 
 function draw() {
   background(5, 5, 5, 25); //makes the background less transparent
-  runObjects();
+  runObjects();//calls the run objects function
 }
 
 function loadObjects(x){
-  planet = new Planet(100, 100, random (-2,2), random(-2, 2), 1); //add main ball
-  //planet2 = new Ball(random(width/2), random(height/2), random (-2,2), random(-2, 2), 1); //add 2nd main ball
-  //planet2 = new Ball(random(width/2), random(height/2), random (-2,2), random(-2, 2), 1); //add 2nd main ball
-  for(var i = 0; i < x; i++){
+  planet = new Planet(100, 100, random (-2,2), random(-2, 2), 1); //creates a planet
+  for(var i = 0; i < x; i++){//creates the ship(s)
     ships[i] = new Ship(100, 200, random(-5,5), random(-5,5), i + 3);
   }
 }
 
 function runObjects(){
-  planet.run();
-  //planet2.run();
-  for(var i = 0; i < ships.length; i++){
+  planet.run();//makes the planet appear
+  for(var i = 0; i < ships.length; i++){//makes the ship(s) appear
     ships[i].run();
 }
 }
